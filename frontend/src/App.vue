bc1ad58d3b4c50b2d6120ecb6cc0c30b94bcf0b9
StanisLove <sta-tun-slav@yandex.ru>
2021-08-24 02:20:45 +0300

    end nav and start main_page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <nav-el />
+    <router-view />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavEl from "./components/nav.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    NavEl,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  display: flex;
+  flex-direction: column;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0 auto;
+  padding: 0;
 }
 </style>
---
+++ frontend/src/components/nav.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="nav">
+    <ul>
+      <li><router-link to="/">О нас</router-link></li>
+      <li><router-link to="/marketplace">MarketPlace</router-link></li>
+      <li><a href="#">Форум</a></li>
+      <li><a href="#">Инструкция</a></li>
+      <li><a href="#">Подбор</a></li>
+      <li>
+        <a href="#">
+          <el-row
+            ><el-button type="warning" plain round class="lk-button"
+              >Войти</el-button
+            ></el-row
+          >
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap");
+.nav {
+  font-family: "Montserrat", sans-serif;
+  display: flex;
+  justify-content: flex-end;
+
+  ul {
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+    align-items: baseline;
+    // border-right: 0.5px black solid;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    list-style-type: none;
+    margin-right: 40px;
+
+    li a {
+      margin-right: 40px;
+      color: #2c3e50;
+      text-decoration: none;
+      font-size: 20px;
+    }
+    li a:hover {
+      border-bottom: 2px rgba(252, 112, 97, 0.8) solid;
+    }
+  }
+}
+.lk-button {
+  width: 150px;
+  height: 50px;
+  position: relative;
+}
+</style>
